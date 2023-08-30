ea79de6fe45b5ef4ad3905c3a60b6848de30a557
m1stLe <merikovk@mail.ru>
2023-08-30 20:01:46 +0300

    не понимаю как исправить 34 задачу

---
+++ Task_34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+void FillArrayRandomNumbers(int[] arr) 
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(99, 999);
+    }
+}
+
+int ReadInt(string message)  
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
