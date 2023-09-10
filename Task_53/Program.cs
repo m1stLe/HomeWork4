d33f399334932c86ea9d390215ba5e371182c135
m1stLe <merikovk@mail.ru>
2023-09-10 17:58:03 +0300

    + 53

---
+++ Task_53/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+int[,] CreateMatrixRndInt(int rows, int colunns, int min, int max)
+{
+    int[,] matrix = new int[rows, colunns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j],6}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void ReplacementSeats(int[,] matrix)
+{
+    int firstRow = 0;
+    int lastRow = matrix.GetLength(0) - 1;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[firstRow, j];
+        matrix[firstRow, j] = matrix[lastRow, j];
+        matrix[lastRow, j] = temp;
+    }
+}
+
+int[,] createMatrixRndInt = CreateMatrixRndInt(3, 4, -100, 100);
+PrintArray(createMatrixRndInt);
+Console.WriteLine();
+ReplacementSeats(createMatrixRndInt);
+PrintArray(createMatrixRndInt);
